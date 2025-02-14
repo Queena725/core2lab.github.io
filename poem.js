14b7f000cd2205bcea2c5718308708fc08d5bd6a
Adi_factorial <71246129+Batsy24@users.noreply.github.com>
2025-02-14 09:55:14 -0500

    Update poem.js

--- poem.js
+++ poem.js
@@ -3,4 +3,18 @@ document.querySelectorAll('.poem li').forEach((line) => {
     const y = Math.random() * window.innerHeight * 0.8;
     line.style.transform = `translate(${x}px, ${y}px)`;
   });
-  
+
+/*
+Hi Queena!!
+
+             ,-._
+           _.-'  '--.
+         .'      _  -`\_
+        / .----.`_.'----'
+        ;/     `
+       /_;
+
+    ._      ._      ._      ._
+_.-._)`\_.-._)`\_.-._)`\_.-._)`\_.-._
+
+*/
